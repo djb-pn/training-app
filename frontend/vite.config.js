480fb5ef34c33b74547afdaf15b2c72764e8143f
djb-pn <darin@picksgallery.com>
2026-02-03 07:59:08 -0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Changing to './' allows the app to load from any directory or domain
-  base: './', 
+  // Since you are using training.picksgallery.com, 
+  // the app lives at the root ('/')
+  base: '/', 
 })
